bafa478a6b3d08491cb0152e98c08bb9a0bc8ecf
jf <jf@tets.com>
2025-03-05 19:23:19 -0500

    se maqueta productos y como listarlos

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, input, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -7,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
+  //img = 'https://picsum.photos/640/640?r=' + Math.random()
+  @Input({required: true}) img: string = ''
+  @Input({required: true}) price: number = 0
+  @Input({required: true}) title: string = ''
 
+  @Output() addToCart = new EventEmitter
+
+  addToCartHandler(){
+    console.log("click from child")
+    this.addToCart.emit('prueba mensaje desde hijo: ' + this.title)
+  }
 }
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 
+import { ProductComponent } from './../../components/product/product.component'
+
 @Component({
   selector: 'app-list',
-  imports: [],
+  imports: [ProductComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
 export class ListComponent {
-
+  fromChild(event: string){
+    console.log('ingresa a metodo padre')
+    console.log(event)
+  }
 }
