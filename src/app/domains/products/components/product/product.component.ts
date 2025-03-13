9b5b374b8bc91fa57508eeddd25a88d8cd5b2a7b
jf <jf@tets.com>
2025-03-12 23:42:19 -0500

    manejo de pipes

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,9 +1,12 @@
 import { Component, EventEmitter, input, Input, Output } from '@angular/core';
 import { Product } from '../../../shared/models/product.model';
+import { CommonModule } from '@angular/common';
+import { ReversePipe } from '@shared/pipes/reverse.pipe';
+import { TimeAgoPipe } from '@shared/pipes/time-ago.pipe';
 
 @Component({
   selector: 'app-product',
-  imports: [],
+  imports: [CommonModule, ReversePipe, TimeAgoPipe],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
