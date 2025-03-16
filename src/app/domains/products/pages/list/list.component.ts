57757820ea952900d8fad30ae2461d45aa4c4c40
jf <jf@tets.com>
2025-03-15 22:13:55 -0500

    agrego prefetching y actualizo sintaxis

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, Input, signal, SimpleChanges } from '@angular/core';
 
 import { ProductComponent } from '@products/components/product/product.component'
 import { Product } from '@shared/models/product.model';
-import { CommonModule } from '@angular/common';
+
 import { CartService } from '@shared/services/cart.service';
 import { ProductService } from '@shared/services/product.service';
 import { CategoryService } from '@shared/services/category.service';
@@ -11,7 +11,7 @@ import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-list',
-  imports: [ProductComponent, CommonModule, RouterLinkWithHref],
+  imports: [ProductComponent, RouterLinkWithHref],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
